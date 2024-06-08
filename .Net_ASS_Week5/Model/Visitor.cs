e9d9ebb532296f94d90c15377052d9f098c38f13
ramya1117 <158478557+ramya1117@users.noreply.github.com>
2024-06-08 23:37:09 +0530

    Add files via upload

---
+++ .Net_ASS_Week5/Model/Visitor.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json;
+namespace VisitorSystem.Model
+{
+    public class Visitor
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("companyName")]
+        public string CompanyName { get; set; }
+
+        [JsonProperty("purpose")]
+        public string Purpose { get; set; }
+
+        [JsonProperty("entryTime")]
+        public DateTime EntryTime { get; set; }
+
+        [JsonProperty("exitTime")]
+        public DateTime ExitTime { get; set; }
+
+        [JsonProperty("passStatus")]
+        public bool PassStatus { get; set; }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+    }
+}
